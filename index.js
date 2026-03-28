7d2f89cf30b48948709b6a033a696216fb7ed2f6
Kandy1116 <venus.kandy@gmail.com>
2026-03-28 13:19:30 -0500

    chore: final user adjustments

--- index.js
+++ index.js
@@ -59,7 +59,13 @@ function toggleModal(section = 'about') {
   }
 }
 
+function openMenu() {
+    document.body.classList += " menu--open";
+}
 
+function closeMenu() {
+    document.body.classList.remove("menu--open");
+}
 
 window.onload = () => {
   function moveBackground(event) {
